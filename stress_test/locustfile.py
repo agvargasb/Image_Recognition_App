6bdbe99805be1735abb8b1f12103ae3976076ae5
Andrés Vargas <agvargasb@gmail.com>
2023-11-07 15:21:55 -0500

    Added locust tests. The dog.jpeg file will change whenever the predict task is run by locust.

--- stress_test/locustfile.py
+++ stress_test/locustfile.py
@@ -1,4 +1,5 @@
 from locust import HttpUser, between, task
+from wand.image import Image
 
 
 class APIUser(HttpUser):
@@ -7,4 +8,17 @@ class APIUser(HttpUser):
     # Put your stress tests here.
     # See https://docs.locust.io/en/stable/writing-a-locustfile.html for help.
     # TODO
-    raise NotImplementedError
+    
+    @task
+    def index(self):
+        self.client.get("http://0.0.0.0/")
+
+    def noise(self):
+        with Image(filename="dog.jpeg") as img:
+            img.noise("gaussian")
+            img.save(filename="dog.jpeg")
+
+    @task
+    def predict(self):
+        self.noise()
+        self.client.post("http://0.0.0.0/predict", files={"file": ("dog.jpeg", open("dog.jpeg", "rb"), "image/jpeg")})
